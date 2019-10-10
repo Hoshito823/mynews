5a7915c43bcef085ae1218eb14d395ed222e81e7
Hoshito Nakamura <otihsoh823@gmail.com>
2019-10-10 13:48:03 +0000

    third commit for practice9

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+//課題3
+Route::get('XXX','AAAController@bbb');
+
+
+Route::group(['prefix' => 'admin'],function (){
+    Route::get('news/create','Admin\NewsController@add');
+    
+//課題4
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+}
+);
+
 Route::get('/', function () {
     return view('welcome');
 });

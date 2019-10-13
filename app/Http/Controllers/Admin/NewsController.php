aeb2b5c82a80c6a4801f1bb37f1490c01837a785
Hoshito Nakamura <otihsoh823@gmail.com>
2019-10-13 15:31:25 +0000

    first commit for practice14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\News;
+
 class NewsController extends Controller
 {
     public function add(){
@@ -12,6 +14,32 @@ class NewsController extends Controller
     }
     
     public function create(Request $request){
+        
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        //フォームの画像が送信されてきたら、保存する$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])){
+            //画像本体をpublic/imageフォルダに保存する？
+            $path = $request->file('image')->store('public/image');
+            
+            //newsテーブルの"image_path"カラムにパスを保存している？
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;     
+        }
+        
+        //フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        //フォームから送信されてきた_tokenを削除する
+        unset($form['image']);
+        
+        //DBにformの内容を保存する（$formはすでに画像等は削除された状態）
+        $news->fill($form);
+        $news->save();
+        
         //admin/news/createにリダイレクトする
         return redirect('admin/news/create');    
     }
---
+++ database/migrations/2019_10_13_114608_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //マイグレーション時の挙動
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('body');
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    //マイグレーションの取り消し（ロールバック）
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}

7729fd5e0e45f17fa58c948525048d428fe25c9d
Hoshito Nakamura <otihsoh823@gmail.com>
2019-10-14 03:39:58 +0000

    first commit for practice14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add(){
@@ -12,6 +14,15 @@ class ProfileController extends Controller
     }
     
     public function create(Request $request){
+        
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
     
